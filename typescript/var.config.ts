e9c9216a3a8e952748364234d591df02763e0f44
Aslak Hellesøy <aslak@oselvar.com>
2026-07-02 16:27:45 +0100

    Doc tweaks

--- typescript/var.config.ts
+++ typescript/var.config.ts
@@ -1,8 +1,6 @@
 export default {
   vars: {
     include: ['packages/var-examples/**/*.md'],
-    // yahtzee.broken.md is a deliberately broken example; it must not run as a spec.
-    exclude: ['packages/var-examples/yahtzee/yahtzee.broken.md'],
   },
   steps: ['packages/var-examples/**/*.steps.ts'],
 }
